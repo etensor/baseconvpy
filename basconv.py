f874143b4d81bcb609bb171a378d9e1970a806ac
etensor <dpenillac@correo.usbcali.edu.co>
2022-03-09 02:31:16 -0500

    derivadas mas elegantes que nunca:

--- basconv.py
+++ basconv.py
@@ -3,7 +3,12 @@ import numpy as np
 from convb import convert_decbase
 from derivadas import *
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    layout="wide",
+    page_icon='🛠',
+    initial_sidebar_state='expanded',
+    menu_items={'About': "### Github:\n www.github.com/etensor/baseconvpy"}
+    )
 
 
 def texbox(text):
@@ -23,14 +28,15 @@ def campo_numeros(numeros):
 
 
 opt_menu = st.sidebar.selectbox(
-    "Navegador",
+    "Navegador del proyecto",
     ("Presentacion","Convertidor de Bases","Derivadas")
 )
 if opt_menu == "Presentacion":
-    st.title('Codigo Fuente del Proyecto')
+    st.title('Métodos Númericos')
+    st.subheader('Calculadoras')
     st.markdown(r'''
     ---
-    # Equipo: 
+    ### Equipo: 
     ##### Jean Pierre Vargas
     ##### David Penilla Cardona
     ##### Juan Camilo Bolaños
@@ -61,16 +67,45 @@ if opt_menu == "Convertidor de Bases":
 if opt_menu == 'Derivadas':
     st.title('Calculadora de Derivadas')
     calc_derivadas = st.container()
+    
 
     with calc_derivadas:
-        eq_funcion = st.text_input('Ingrese función: ')
+        col_eq,col_diff = st.columns([4,1])
+
+        with col_eq:
+            st.write('\n')
+            st.write('\n')
+            eq_funcion = st.text_input(
+                'Ingrese función: ', value='cos(xy) + 3x**3 y**-3 z**2 - sin(xz)')
+            diff_var = ''
+
+        with col_diff:
+            tipo_diff = st.radio(
+                '',
+                ('simple','libre'))
+            
+            if tipo_diff == 'simple':
+                diff_var = st.selectbox(
+                    'Diferenciar sobre:',
+                    ('x','y','z','t','r','v','w'),
+                )
+            else:
+                diff_var = st.text_input('Respecto a qué variable derivar, y cuantas veces:',
+                    value='x,2 ; y ; z ; z',
+                    help='Utilice ; para separar argumentos, \n puede diferenciar n veces x y luego z asi: x,n;z')
+                diff_var = diff_var.split(';')
+
         with st.expander(' Derivadas ',True):
             
-            derivadas = derivadasFuncion(eq_funcion, x)
+            derivadas = derivadasFuncion(eq_funcion, *diff_var)
+            col_expr,col_spc,col_plots = st.columns(3)
+
+
             for dfdx in derivadas:
-                st.latex(f"{dfdx[0]} \quad = \quad {dfdx[1]}")
+                col_expr.latex(f"{dfdx[0]} \quad = \; {dfdx[1]}")
+                #col_deriv.latex(f"{dfdx[1]}")
+            #    st.latex(f"{dfdx[0]} \quad = \quad {dfdx[1]}")
         
-            
 
 
 
